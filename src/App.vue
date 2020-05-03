3f4aa9d6a7012a31f5ec70d7475a2566cd783329
Yoyo San <veselu@gmail.com>
2020-05-03 16:53:17 +0300

    App: add language detection and switch it.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,26 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted () {
+    // detect user language
+    switch (this.$q.lang.getLocale().toLowerCase()) {
+      case 'en-us':
+      case 'en-uk':
+        this.$i18n.locale = 'en-us'
+
+        break
+      case 'ro-ro':
+      case 'ro-md':
+      case 'ro':
+        this.$i18n.locale = 'ro-ro'
+
+        break
+      default:
+        this.$i18n.locale = 'en-us'
+
+        break
+    }
+  }
 }
 </script>
