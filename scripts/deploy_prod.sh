33fc810e95b44b00aa36e37ce835b6bd4faed0a6
Marius Palade <contact@mpalade.ro>
2021-07-30 08:47:11 +0300

    scripts: append sudo for changing permissions

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -5,5 +5,5 @@ yarn
 quasar build
 cp .htaccess dist/spa
 cp robots.txt dist/spa
-chown -R worker:www-data .
-chmod -R u+rwX,go+rX,go-w .
+sudo chown -R worker:www-data .
+sudo chmod -R u+rwX,go+rX,go-w .
